8876aa72b07df34aeb50f8b6a248240f37538a05
NickMihaiu91 <nick.mihaiu91@gmail.com>
2015-07-15 00:18:15 +0300

    Ordered cities alphabetically

--- js/typeahead-data.js
+++ js/typeahead-data.js
@@ -1,6 +1,6 @@
 ﻿(function () {
 
-    var canadaCitiesList = ["Vancouver", "Richmond", "Coquitlam", "Port Coquitlam", "North Vancouver", "Burnaby", "Delta", "Langley", "Lions Bay", "Maple Ridge", "New Westminster", "Pitt Meadows", "Port Moody", "Surrey", "West Vancouver", "White Rock"];
+    var canadaCitiesList = ["Vancouver", "Burnaby", "Coquitlam", "Delta", "Langley", "Lions Bay", "Maple Ridge", "New Westminster", "North Vancouver", "Pitt Meadows", "Port Coquitlam", "Port Moody", "Richmond", "Surrey", "West Vancouver", "White Rock"];
 
     $(document).ready(function () { 
         $("#inputLocation").typeahead({ source: canadaCitiesList });
