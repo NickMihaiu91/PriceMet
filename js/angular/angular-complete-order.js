c08589e4a56df89400873d3d3433dea1504254e7
NickMihaiu91 <nick.mihaiu91@gmail.com>
2015-08-16 17:06:09 +0300

    Complete order - button clicked

--- js/angular/angular-complete-order.js
+++ js/angular/angular-complete-order.js
@@ -19,10 +19,40 @@
             });
         }, 300);
 
+        $scope.completeOrder = function () {
+            $('#completeOrderModal').modal({ backdrop: 'static' });
+        };
+
         $scope.$watch('quantity', function () {
             if ($scope.offer)
                 $scope.total = $scope.offer.offerPrice * $scope.quantity;
         });
+    });
+
+    priceMetApp.controller('CompleteOrderModalCtrl', function ($scope, $timeout) {
+        $scope.showErrorMsg = false;
+
+        $('#completeOrderModal').on('show.bs.modal', function () {
+            $scope.showErrorMsg = false;
+            $scope.showEmailError = false;
+            $scope.sentEmailAddress = false;
+
+            $timeout(function () {
+                $scope.showErrorMsg = true;
+            }, 5000);
+        });
+
+        $scope.sendEmail = function () {
+            var validEmail = validateEmail($scope.email);
+
+            $scope.showEmailError = false;
+
+            if (!validEmail)
+                return $scope.showEmailError = true;
+
+            $scope.sentEmailAddress = true;
+            saveEmail($scope.email);
+        };
     });
 
     function getParameterByName(name) {
@@ -35,5 +65,19 @@
     function formatNoOfPersonsToTextRepresentation(noOfPersons) {
         var textValues = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
         return textValues[noOfPersons];
+    }
+
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        return re.test(email);
+    }
+
+    function saveEmail(email) {
+        var EmailObject = Parse.Object.extend("Email"),
+            emailObject = new EmailObject();
+
+        emailObject.save({
+            email: email
+        });
     }
 })();
